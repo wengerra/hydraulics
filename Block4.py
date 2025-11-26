1b2624dcdcc2ce3ce38edafbd2bc886144544e4c
wengerra <wengerr@ethz.ch>
2025-11-26 10:48:12 +0100

    Block4.py

--- Block4.py
+++ Block4.py
@@ -26,4 +26,46 @@ L_ref = 30
 D_ref = 0.3
 
 ref_curve = delta_H(lambda_ref, L_ref, D_ref, Q_range)
-var_curve = delta_H(lambda
+var_curve = delta_H(lambda_val, L_val, D_val, Q_range)
+hr_point = delta_H(lambda_val, L_val, D_val, Q_point)
+
+# ---------- Plot ----------
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Formel oben
+fig.suptitle(
+    r'$h_r = \lambda \cdot \frac{L}{D} \cdot \frac{v^2}{2g} '
+    r'= \lambda \cdot \frac{L}{D} \cdot \frac{\left(\frac{4Q}{\pi D^{2}}\right)^{2}}{2g} '
+    r'= \frac{8 \cdot \lambda \cdot L \cdot Q^2}{g \cdot \pi^2 \cdot D^5}$',
+    fontsize=16,
+    y=0.98
+)
+
+# Kurven
+ax.plot(Q_range, ref_curve, "k--", label="Referenzkurve")
+ax.plot(Q_range, var_curve, "b", label="Aktuelle Kurve")
+ax.plot(Q_point, hr_point, "ro", markersize=8)
+
+# Punktbeschriftung
+ax.text(
+    Q_point,
+    hr_point + 0.3,
+    f"Q = {Q_point:.2f} m³/s\n$h_r$ = {hr_point:.1f} m",
+    fontsize=12,
+    color="red",
+    ha="left",
+    va="bottom",
+    bbox=dict(facecolor="white", edgecolor="red", boxstyle="round,pad=0.2")
+)
+
+ax.set_xlabel("Durchfluss Q [m³/s]")
+ax.set_ylabel("Verlusthöhe $h_r$ [m]")
+ax.grid(True)
+ax.legend()
+
+# ---------- Fixierte Achsen ----------
+ax.set_xlim(0, 0.5)
+ax.set_ylim(0, np.max(ref_curve)*1.2)  # leicht oberhalb Referenzkurve
+
+st.pyplot(fig)
+
