84896676b7ad465b53ba0b26c1564303edec57ad
wengerra <wengerr@ethz.ch>
2025-11-26 20:37:45 +0100

    Update Block4.py

--- Block4.py
+++ Block4.py
@@ -17,7 +17,7 @@ def delta_H(lmbda, L, D, Q):
 st.sidebar.header("Parameter")
 lambda_val = st.sidebar.slider("λ (Reibungsbeiwert)", 0.01, 0.05, 0.03, 0.001)
 L_val      = st.sidebar.slider("L (Rohrlänge) [m]", 10, 50, 30, 5)
-D_val      = st.sidebar.slider("D (Durchmesser) [m]", 0.25, 0.3, 0.5, 0.025)
+D_val      = st.sidebar.slider("D (Durchmesser) [m]", 0.25, 0.5, 0.3, 0.025)
 Q_point    = st.sidebar.slider("Q (Punkt) [m³/s]", 0.0, 0.5, 0.3, 0.025)
 
 # --- Werte für Plot ---
@@ -69,6 +69,7 @@ ax.set_ylim(0, 25)
 
 # --- Plot anzeigen ---
 st.pyplot(fig)
+
 
 
 
