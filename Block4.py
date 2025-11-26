18d5f2cf38057abfcc10fa7e2870e6ab382af32e
wengerra <wengerr@ethz.ch>
2025-11-26 20:48:19 +0100

    Update Block4.py

--- Block4.py
+++ Block4.py
@@ -39,9 +39,9 @@ fig.suptitle(
     r'= \lambda \cdot \frac{L}{D} \cdot \frac{\left(\frac{4Q}{\pi D^{2}}\right)^{2}}{2g} '
     r'= \frac{8 \cdot \lambda \cdot L \cdot Q^2}{g \cdot \pi^2 \cdot D^5}$',
     fontsize=16,
-    y=0.99
+    y=2
 )
-fig.tight_layout(rect=[0, 0, 1, 0.92])
+fig.tight_layout(rect=[0, 0, 1, 0.94])
 
 # Kurven
 ax.plot(Q_range, ref_curve, "k--", label="Referenzkurve")
@@ -70,6 +70,7 @@ ax.set_ylim(0, 25)
 
 # --- Plot anzeigen ---
 st.pyplot(fig)
+
 
 
 
